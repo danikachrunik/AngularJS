27ea91f7138c129a2814fb1a3b8e09e5e7669a2e
Chrunik <dchrunik@contractor.ea.com>
2016-10-28 09:24:25 -0700

    added module1 solution

---
+++ mod1-solution/app.js
@@ -0,0 +1,51 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.foods = "";
+  $scope.message = "";
+  $scope.hide = "hidden";
+  $scope.textColor = "";
+
+  $scope.dispalyMessage = function () {
+    //reset textColor and hide info box
+    $scope.hide = "hidden";
+    $scope.textColor = "";
+
+    //make an array of foods from user input
+    var foodsList = $scope.foods.split(',');
+
+    //check the length of items in array
+    function isEmpty(elem, index, array) {
+      return elem.trim().length == 0;
+    }
+
+    //check for an empty input
+    if(foodsList.every(isEmpty)) {
+      $scope.textColor = "text-danger";
+      $scope.message = "Please enter a list of foods you like to eat for lunch.";
+    } else {
+      if(foodsList.some(isEmpty)) {
+        $scope.hide = "";
+      }
+      //check the number of items if the foodsList array
+      if(foodsList.length <= 3) {
+        $scope.textColor = "text-success";
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.textColor = "text-success";
+        $scope.message = "Too much!";
+      }
+
+    } // end of else
+  } // end of dispalyMessage function
+} // end of LunchCheckController
+
+})();
+//banana, ice-cream, chocolate sprinkles, cherry
+//banana, ice-cream, ,chocolate sprinkles, cherry
